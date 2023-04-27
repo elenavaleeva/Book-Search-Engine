953c9af3c5dec0c8e3dc4825916e31eebc37a93b
ELENA VALEEVA <elenavaleeva05@gmail.com>
2023-04-27 18:12:52 -0400

    more commits

---
+++ 02-Challenge/Develop/client/src/pages/SavedBooks.js
@@ -0,0 +1,91 @@
+import React, { useState, useEffect } from 'react';
+import {
+  Jumbotron,
+  Container,
+  Card,
+  Button,
+  CardColumns,
+  Col,
+  Jumbotron
+} from 'react-bootstrap';
+import { useQuery, useMutation } from '@apollo/react-hooks';
+import { getMe } from '../utils/queries';
+import Auth from '../utils/auth';
+
+import { REMOVE_BOOK } from '../utils/mutations';
+import { removeBookId } from '../utils/localStorage';
+
+
+
+const SavedBooks = () => {
+  const { data } = useQuery(getMe);
+  const [deleteBook] = useMutation(REMOVE_BOOK);
+
+  // use this to determine if `useEffect()` hook needs to run again
+
+  const userData = data?.me || {};
+
+  const userDataLength = Object.keys(userData).length;
+
+  const handleDeleteBook = async (bookId) => {
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+    if (!token) {
+      return false;
+    }
+
+    try {
+      await deleteBook({
+        variables: { bookId }
+      });
+
+      removeBookId(bookId);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  if (!userDataLength) {
+    return <h2>LOADING...</h2>;
+  }
+
+  // create function that accepts the book's mongo _id value as param and deletes the book from the database
+
+  return (
+    <>
+      <Jumbotron fluid className="text-light bg-dark p-5">
+        <Container>
+          <h1>Viewing saved books!</h1>
+        </Container>
+      </Jumbotron>
+      <Container>
+        <h2 className='pt-5'>
+          {userData.savedBooks.length
+            ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
+            : 'You have no saved books!'}
+        </h2>
+        <CardColumns>
+          {userData.savedBooks.map((book) => {
+            return (
+              <Col md="4">
+                <Card key={book.bookId} border='dark'>
+                  {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
+                  <Card.Body>
+                    <Card.Title>{book.title}</Card.Title>
+                    <p className='small'>Authors: {book.authors}</p>
+                    <Card.Text>{book.description}</Card.Text>
+                    <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
+                      Delete this Book!
+                    </Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            );
+          })}
+        </CardColumns>
+      </Container>
+    </>
+  );
+};
+
+export default SavedBooks;
